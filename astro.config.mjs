7b9df45b7001295465f42a4a99107be5e69a32ef
Vinay <vinayb950529@gmail.com>
2023-08-27 19:04:08 +0530

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  site: "https://stargazers.club",
+  integrations: [tailwind(), mdx(), sitemap()],
 });
